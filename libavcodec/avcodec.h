6a4bc6c8152a81254512d4158ec70bbdb968549d
Bjorn Roche <bjorn@giphy.com>
2017-10-24 12:32:32 -0400

    fix for transparencies in animated gifs
    Support for transparencies in animated gifs requires modifying both
    libavcodec/gif.c and libavformat/gif.c because both the graphics
    control extension (handled by libavformat/gif.c) and the raw frame data
    (handled by libavcodec/gif.c) must be changed. This is because
    transparencies in GIF can be used both to create a transparent image,
    and to provide optimization.
    
    How transparencies are interpreted in a given frame is controlled by
    the “disposal method”, which must be set appropriately in the graphics
    control extension.
    
     The “in place” disposal method is used when transparency indicates
    optimization, and the “background” disposal method is used when
    transparency is intended to be preserved. In order to support both
    disposal methods, libavcodec/gif.c must signal to libavformat/gif.c
    which disposal method is required for every frame. This is done with a
    new side data type: AV_PKT_DATA_GIF_FRAME_DISPOSAL. This requires a
    change to avcodec.h
    
    Unfortunately, the addition of a new side data type causes some of the
    FATE tests to fail, so the fate tests are updated here as well.

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1607,6 +1607,12 @@ enum AVPacketSideDataType {
     AV_PKT_DATA_A53_CC,
 
     /**
+     * The disposal method that should be used with the frame. If missing,
+     * the frame will not be disposed. This contains exactly one byte.
+     */
+    AV_PKT_DATA_GIF_FRAME_DISPOSAL,
+
+    /**
      * The number of side data elements (in fact a bit more than it).
      * This is not part of the public API/ABI in the sense that it may
      * change when new side data types are added.
